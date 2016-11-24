bea33ad18c35346891d318c9f3641d18f6804c00
book777 <book777@users.noreply.github.com>
2016-11-24 19:03:08 +0100

    v2

--- random.h
+++ random.h
@@ -1,5 +1,3 @@
-// If _nanoCHANGE is not defined, randomize seed has been changed every second
-//#define _nanoCHANGE// If defined, randomize seed has been changed every nanosecond
 #ifndef _H_RANDOM
   #define _H_RANDOM
 
@@ -10,30 +8,10 @@
   {
     if(min==max)
       return min;
-
-    #ifdef _nanoCHANGE
-      struct timespec curtime;
-      clock_gettime(CLOCK_MONOTONIC_RAW, &curtime);
-      srand(curtime.tv_nsec);//Randomize rand seed
-    #else
-      srand(time(NULL));//Randomize rand seed
-    #endif
-
     if(min>max)
-      return (rand() % (min-max+1)) +max;
+      return (rand()%(min-max+1))+max;
     else
-      return (rand() % (max-min+1)) +min;
-  }
-
-  int bl_random() {
-    #ifdef _nanoCHANGE
-      struct timespec curtime;
-      clock_gettime(CLOCK_MONOTONIC_RAW, &curtime);
-      srand(curtime.tv_nsec);//Randomize rand seed
-    #else
-      srand(time(NULL));//Randomize rand seed
-    #endif
-    return rand();
+      return (rand()%(max-min+1))+min;
   }
 
 #endif
